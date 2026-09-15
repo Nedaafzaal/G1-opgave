25ad6a795e23f8899d60fcf19ad23aa847e296d3
Nedaafzaal <neaf24ab@student.cbs.dk>
2026-09-15 16:12:16 +0200

    Ny

--- min_app/App.js
+++ min_app/App.js
@@ -1,15 +1,18 @@
+//Her imoteres useSate så det er muligt at gemme og ændre listen af opgaver
 import { useState } from 'react';
+//Impoteres så man kan bruge navigation
 import { NavigationContainer } from '@react-navigation/native';
+//Bruges til at oprette Stack navigation mellem de 3 screens 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+//Her impoteres Homescreen, taskscreen og addtaskscreen. 
 import HomeScreen from './screens/HomeScreen';
 import TasksScreen from './screens/TasksScreen';
 import AddTaskScreen from './screens/AddTaskScreen';
-
+//Her oprettes stack navigator
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-
+//Her bliver listen af opgaver gemt i state
   const [tasks, setTasks] = useState([
     { id: '1', title: 'Læs kapitel 4' },
     { id: '2', title: 'Lav aflevering' },
@@ -33,7 +36,7 @@ export default function App() {
           {(props) => (
             <TasksScreen
               {...props}
-              tasks={tasks}
+              tasks={tasks}//Her sendes listen af opgaver videre til taskScreen. 
             />
           )}
         </Stack.Screen>
@@ -45,8 +48,8 @@ export default function App() {
           {(props) => (
             <AddTaskScreen
               {...props}
-              tasks={tasks}
-              setTasks={setTasks}
+              tasks={tasks}//Her sendes den nuværende liste af opgaver videre
+              setTasks={setTasks}//Her bliver listen opdateret ved at sende funktionen videre. 
             />
           )}
         </Stack.Screen>
