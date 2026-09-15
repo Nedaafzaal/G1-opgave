fe1cca06e8fa88678913ef965766b48755f5ecc6
Nedaafzaal <neaf24ab@student.cbs.dk>
2026-09-15 16:20:18 +0200

    ny

--- min_app/screens/TasksScreen.js
+++ min_app/screens/TasksScreen.js
@@ -1,6 +1,8 @@
+//Her bliver der impoteret view,text og FlatList.
+//FlatList bliver anvendt til at se listen af opgaver.
 import { View, Text, FlatList } from 'react-native';
+//Her bliver stylingen impoteret fra Style.js
 import styles from '../styles/Style';
-
 export default function TasksScreen({ tasks }) {
   return (
     <View style={styles.container}>
@@ -8,7 +10,6 @@ export default function TasksScreen({ tasks }) {
       <Text style={styles.title}>
         Mine opgaver
       </Text>
-
       <FlatList
         data={tasks}
         keyExtractor={(item) => item.id}
